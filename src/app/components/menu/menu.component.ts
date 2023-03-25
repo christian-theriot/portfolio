a8d82c2c46bf12d26e17980a7e3b8bbc25277791
christian-theriot <christian@theriot.dev>
2023-03-25 15:23:24 -0400

    Add form-floating class; setup sign up form on create profile page

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
--- src/app/components/pages/profile-pages/create-profile-page/create-profile-page.component.ts
+++ src/app/components/pages/profile-pages/create-profile-page/create-profile-page.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+
+function matchOtherValidator(otherName: string): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const other = control.root.get(otherName);
+
+    if (other && control.value !== other.value) {
+      return { 'match other': true };
+    }
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-create-profile-page',
   templateUrl: './create-profile-page.component.html',
-  styleUrls: ['./create-profile-page.component.scss']
+  styleUrls: ['./create-profile-page.component.scss'],
 })
 export class CreateProfilePageComponent {
+  form = this.fb.group({
+    email: this.fb.control(null, {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: this.fb.control(null, {
+      validators: [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(
+          /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}/
+        ),
+      ],
+    }),
+    'validate password': this.fb.control(null, {
+      validators: [Validators.required, matchOtherValidator('password')],
+    }),
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+
+    console.log(this.form.controls);
+  }
 }
